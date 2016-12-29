2826c161ffc0528f11500a3df31ff000a1c261dd
djdolls <hdandroid16@gmail.com>
2016-12-29 13:15:44 +0530

    again DJ Rocks

--- app/src/main/java/com/rcoem/enotice/enotice_app/AdminApprove.java
+++ app/src/main/java/com/rcoem/enotice/enotice_app/AdminApprove.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -20,6 +21,7 @@ public class AdminApprove extends AppCompatActivity {
     private Button Approved;
     private Button Rejected;
     private boolean process;
+    private ImageButton mViewImage;
     private DatabaseReference mDatabase;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,11 +31,14 @@ public class AdminApprove extends AppCompatActivity {
         setContentView(R.layout.activity_admin_approve);
         mPostTitle = (TextView) findViewById(R.id.username);
         mDatabase = FirebaseDatabase.getInstance().getReferenceFromUrl(str);
+        mViewImage = (ImageButton) findViewById(R.id.select_image_Button1);
 
         mDatabase.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
                 mPostTitle.setText(dataSnapshot.child("name").getValue().toString().trim());
+                String imageUrl = dataSnapshot.child("images").getValue().toString().trim();
+                Picasso.with(AdminApprove.this).load(imageUrl).into(mViewImage);
 
             }
 
@@ -53,6 +58,7 @@ public class AdminApprove extends AppCompatActivity {
                     public void onDataChange(DataSnapshot dataSnapshot) {
                         if(process) {
                             mDatabase.child("level").setValue(2);
+                            mDatabase.child("DEST").setValue("HOD");
                             process = false;
 
                             Toast.makeText(AdminApprove.this, "HOD", Toast.LENGTH_LONG).show();
@@ -79,12 +85,14 @@ public class AdminApprove extends AppCompatActivity {
                     public void onDataChange(DataSnapshot dataSnapshot) {
                         if(process) {
                             mDatabase.child("level").setValue(1);
+                            mDatabase.child("DEST").setValue("AP");
                             process = false;
 
                             Toast.makeText(AdminApprove.this, "Assistant Professor", Toast.LENGTH_LONG).show();
                             Intent intent = new Intent(AdminApprove.this, AccountAdminPanel.class);
                             startActivity(intent);
                             finish();
+                            ///
                         }
                     }
 
