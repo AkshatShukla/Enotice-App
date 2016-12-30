6e30ea17788fbbbeedaf25d6f3e3a11cb6a8939c
Akshat <aks.shukla47@gmail.com>
2016-12-31 03:32:32 +0530

    Added Testing Indicator for Images
    Indicates whether the image notice is fetched from Network, Disk or
    Memory.

--- app/src/main/java/com/rcoem/enotice/enotice_app/EnoticeApp.java
+++ app/src/main/java/com/rcoem/enotice/enotice_app/EnoticeApp.java
@@ -23,6 +23,7 @@ public class EnoticeApp extends Application {
         Picasso.Builder builder = new Picasso.Builder(this);
         builder.downloader(new OkHttpDownloader(this,Integer.MAX_VALUE));
         Picasso built = builder.build();
+        built.setIndicatorsEnabled(false); //True only for Testing purpose
         built.setLoggingEnabled(true);
         Picasso.setSingletonInstance(built);
 
