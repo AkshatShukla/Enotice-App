701d2e04c2cba79308501d975be24c5b88380267
Akshat <aks.shukla47@gmail.com>
2017-02-14 17:40:08 +0530

    Add Notice Fragments UI changes BETA
    Changes UI to incorporate tabbed activity for notice generation

--- app/src/main/java/com/rcoem/enotice/enotice_app/AddImageNoticeFragment.java
+++ app/src/main/java/com/rcoem/enotice/enotice_app/AddImageNoticeFragment.java
@@ -25,6 +25,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.Toast;
 import android.widget.VideoView;
@@ -34,6 +35,7 @@ import com.theartofdev.edmodo.cropper.CropImageView;
 
 import static android.app.Activity.RESULT_CANCELED;
 import static android.app.Activity.RESULT_OK;
+import static com.bumptech.glide.gifdecoder.GifHeaderParser.TAG;
 
 
 /**
@@ -44,12 +46,20 @@ public class AddImageNoticeFragment extends Fragment  {
 
     // Camera activity request codes
     // Activity result key for camera
-    static final int REQUEST_TAKE_PHOTO = 11111;
+    // Camera activity request codes
+    private static final int CAMERA_CAPTURE_IMAGE_REQUEST_CODE = 100;
+    private static final int CAMERA_CAPTURE_VIDEO_REQUEST_CODE = 200;
+
+    public static final int MEDIA_TYPE_IMAGE = 1;
+    public static final int MEDIA_TYPE_VIDEO = 2;
+
     private Uri mImageUri = null;
     private static final int Gallery_Request = 1;
 
-    private Button btnCapturePicture;
+    private Button btnCaptureImage;
+    private Button btnChooseImage;
     private ImageView imgPreview;
+    private static final int CAMERA_REQUEST = 1888;
     private Uri fileUri; // file url to store image/video
 
     // directory name to store captured images and videos
@@ -83,49 +93,80 @@ public class AddImageNoticeFragment extends Fragment  {
 
     public void onStart(){
         super.onStart();
-        Button bt = (Button)context.findViewById(R.id.btnCapturePicture);
-        bt.setOnClickListener(new View.OnClickListener(){
+        btnCaptureImage = (Button) context.findViewById(R.id.btnCapturePicture);
+        btnCaptureImage.setOnClickListener(new View.OnClickListener(){
             public void onClick(View view){
 
                /* //create an Intent object
                 Intent intent = new Intent(context, AddNoticeActivityAdmin.class);
 
                 //start the second activity
-                startActivity(intent); */
+                startActivity(intent);
 
                 Intent galleryIntent = new Intent(Intent.ACTION_GET_CONTENT);
                 galleryIntent.setType("image/*");
                 startActivityForResult(galleryIntent , Gallery_Request);
+                */
+                Intent cameraIntent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(cameraIntent, CAMERA_REQUEST);
             }
 
         });
+        btnChooseImage = (Button) context.findViewById(R.id.btnChooseImage);
+        btnChooseImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(
+                        Intent.ACTION_PICK,
+                        android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+
+                startActivityForResult(i, Gallery_Request);
+            }
+        });
+
 
         imgPreview  = (ImageView) context.findViewById(R.id.imagePreview);
+
     }
 
     @Override
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        if (requestCode == Gallery_Request && resultCode == RESULT_OK) {
-            Uri ImageUri = data.getData();
-            CropImage.activity(ImageUri)
-                    .setGuidelines(CropImageView.Guidelines.ON)
-                    .start(context);
-
+        if (requestCode == CAMERA_REQUEST && resultCode == Activity.RESULT_OK) {
+            Bitmap photo = (Bitmap) data.getExtras().get("data");
+            imgPreview.setImageBitmap(photo);
         }
-        if (requestCode == CropImage.CROP_IMAGE_ACTIVITY_REQUEST_CODE) {
-            CropImage.ActivityResult result = CropImage.getActivityResult(data);
-            if (resultCode == RESULT_OK) {
 
-                mImageUri = result.getUri();
+        try {
+            // When an Image is picked
+            if (requestCode == Gallery_Request && resultCode == Activity.RESULT_OK
+                    && null != data) {
+                Uri selectedImage = data.getData();
 
-                imgPreview.setImageURI(mImageUri);
-
-            } else if (resultCode == CropImage.CROP_IMAGE_ACTIVITY_RESULT_ERROR_CODE) {
-                Exception error = result.getError();
+                CropImage.activity(selectedImage)
+                        .setGuidelines(CropImageView.Guidelines.ON)
+                        .start(getContext(),this);
             }
+            // when image is cropped
+            else if (requestCode == CropImage.CROP_IMAGE_ACTIVITY_REQUEST_CODE) {
+                CropImage.ActivityResult result = CropImage.getActivityResult(data);
+                Log.d("APP_DEBUG",result.toString());
+                if (resultCode == Activity.RESULT_OK) {
+                    Uri resultUri = result.getUri();
+                    Log.d("APP_DEBUG",resultUri.toString());
+                    Bitmap bitmap =  MediaStore.Images.Media.getBitmap(context.getContentResolver(), resultUri);
+                    imgPreview.setImageBitmap(bitmap);
+
+                } else if (resultCode == CropImage.CROP_IMAGE_ACTIVITY_RESULT_ERROR_CODE) {
+                    Exception error = result.getError();
+                }
+            }
+
+        } catch (Exception e) {
+            Toast.makeText(context, "Something went wrong"+e.getMessage(), Toast.LENGTH_LONG)
+                    .show();
         }
     }
+
 }
 
 
