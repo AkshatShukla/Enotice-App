5c02f27ba4285f7c03abb6ccbf5d21fbcf4f04d1
Akshat <aks.shukla47@gmail.com>
2017-02-16 12:16:54 +0530

    Dev Testing for Image Notice upload and Crash Fix
    Fixed OutOfMemory Crash by increasing heap memory in android manifest.
    Added Image notice uploading from new UI

--- app/src/main/java/com/rcoem/enotice/enotice_app/AddImageNoticeFragment.java
+++ app/src/main/java/com/rcoem/enotice/enotice_app/AddImageNoticeFragment.java
@@ -5,10 +5,12 @@ import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
 import java.util.Locale;
 
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
@@ -19,19 +21,40 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
 import android.provider.MediaStore;
+import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
+import android.text.TextUtils;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 import android.widget.VideoView;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 import com.theartofdev.edmodo.cropper.CropImage;
 import com.theartofdev.edmodo.cropper.CropImageView;
+
+import dmax.dialog.SpotsDialog;
+import es.dmoral.toasty.Toasty;
 
 import static android.app.Activity.RESULT_CANCELED;
 import static android.app.Activity.RESULT_OK;
@@ -44,26 +67,32 @@ import static com.bumptech.glide.gifdecoder.GifHeaderParser.TAG;
 
 public class AddImageNoticeFragment extends Fragment  {
 
-    // Camera activity request codes
-    // Activity result key for camera
-    // Camera activity request codes
-    private static final int CAMERA_CAPTURE_IMAGE_REQUEST_CODE = 100;
-    private static final int CAMERA_CAPTURE_VIDEO_REQUEST_CODE = 200;
 
-    public static final int MEDIA_TYPE_IMAGE = 1;
-    public static final int MEDIA_TYPE_VIDEO = 2;
-
-    private Uri mImageUri = null;
+    private StorageReference mStoarge;
+    private DatabaseReference mData;
+    private DatabaseReference mDataUser;
+    private DatabaseReference mDataBaseDepartment;
+    private DatabaseReference mDatabase;
+    private FirebaseAuth mAuth;
+    private FirebaseUser mCurrentUser;
     private static final int Gallery_Request = 1;
 
+    private EditText titleText;
+    private EditText descText;
     private Button btnCaptureImage;
     private Button btnChooseImage;
+    private Button btnSubmit;
     private ImageView imgPreview;
     private static final int CAMERA_REQUEST = 1888;
-    private Uri fileUri; // file url to store image/video
+    private Uri mImageUri = null;
 
     // directory name to store captured images and videos
     private static final String IMAGE_DIRECTORY_NAME = "E Notices";
+
+    private DatePicker datePicker;
+    private Calendar calendar;
+    private TextView dateView;
+    private int year, month, day;
 
     View view;
 
@@ -93,6 +122,29 @@ public class AddImageNoticeFragment extends Fragment  {
 
     public void onStart(){
         super.onStart();
+        mAuth = FirebaseAuth.getInstance();
+        mStoarge = FirebaseStorage.getInstance().getReference();
+        mCurrentUser = mAuth.getCurrentUser();
+
+
+        mDataBaseDepartment = FirebaseDatabase.getInstance().getReference().child("Users").child(mAuth.getCurrentUser().getUid());
+
+        mDataBaseDepartment.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                mData = FirebaseDatabase.getInstance().getReference().child("posts").child(dataSnapshot.child("department").getValue().toString().trim()).child("Deptposts");
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+        mDataUser = FirebaseDatabase.getInstance().getReference().child("Users").child(mCurrentUser.getUid());
+
+        titleText = (EditText) context.findViewById(R.id.titleText);
+        descText = (EditText) context.findViewById(R.id.descText);
+
         btnCaptureImage = (Button) context.findViewById(R.id.btnCapturePicture);
         btnCaptureImage.setOnClickListener(new View.OnClickListener(){
             public void onClick(View view){
@@ -123,10 +175,109 @@ public class AddImageNoticeFragment extends Fragment  {
                 startActivityForResult(i, Gallery_Request);
             }
         });
+        btnSubmit = (Button) context.findViewById(R.id.btnSubmit);
+        btnSubmit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
 
+                Toasty.info(context,"Submitting").show();
+                mDataBaseDepartment = FirebaseDatabase.getInstance().getReference().child("Users").child(mAuth.getCurrentUser().getUid());
+                mDataBaseDepartment.addValueEventListener(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(DataSnapshot dataSnapshot) {
+                        String Dept = dataSnapshot.child("department").getValue().toString().trim();
+                        startPosting(Dept);
+                    }
+
+                    @Override
+                    public void onCancelled(DatabaseError databaseError) {
+
+                    }
+                });
+            }
+        });
 
         imgPreview  = (ImageView) context.findViewById(R.id.imagePreview);
 
+    }
+
+    private void startPosting(final String Dept) {
+        final String title_value =  titleText.getText().toString().trim();
+        final String desc_value = descText.getText().toString().trim();
+        final String user_id =  mAuth.getCurrentUser().getUid();
+        calendar = Calendar.getInstance();
+        year = calendar.get(Calendar.YEAR);
+
+        month = calendar.get(Calendar.MONTH) + 1;    //Month in Calendar API start with 0.
+        day = calendar.get(Calendar.DAY_OF_MONTH);
+        //  Toast.makeText(AddNoticeActivityAdmin.this,day + "/" + month + "/" + year, Toast.LENGTH_LONG).show();
+        final String currentDate = day + "/" + month + "/" + year;
+        final long currentLongTime = -1 * new Date().getTime();
+        final String currentTime = "" + currentLongTime;
+
+
+        //mProgress.setMessage("Uploading Notice...");
+        final AlertDialog dialog = new SpotsDialog(context, R.style.CustomProgress);
+        dialog.show();
+
+        if(!TextUtils.isEmpty(title_value) && !TextUtils.isEmpty(desc_value) && imgPreview.getDrawable() != null){
+            //mProgress.show();
+            StorageReference filepath = mStoarge.child("Images").child(mImageUri.getLastPathSegment());
+            filepath.putFile(mImageUri).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                @Override
+                public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                    final Uri downloadUrl = taskSnapshot.getDownloadUrl();
+
+                    final DatabaseReference newPost =mData.push();
+
+                    mDataUser.addValueEventListener(new ValueEventListener() {
+                        @Override
+                        public void onDataChange(DataSnapshot dataSnapshot) {
+
+                            newPost.child("approved").setValue("true");  //No Authentication is Required.
+                            newPost.child("removed").setValue(0);        //Not removed initially.
+                            newPost.child("title").setValue(title_value);
+                            newPost.child("time").setValue(currentDate);
+                            newPost.child("servertime").setValue(currentLongTime);
+                            newPost.child("Desc").setValue(desc_value);
+                            newPost.child("email").setValue(mAuth.getCurrentUser().getEmail());
+                            newPost.child("department").setValue(Dept);
+                            newPost.child("images").setValue(downloadUrl.toString());
+                            newPost.child("username").setValue(dataSnapshot.child("name").getValue()).addOnCompleteListener(new OnCompleteListener<Void>() {
+                                @Override
+                                public void onComplete(@NonNull Task<Void> task) {
+                                    if(task.isSuccessful()){
+                                        Toasty.success(context,"Upload Successfully").show();
+                                    }
+                                    else
+                                    {
+                                        Toasty.error(context,"Upload Unsuccessful").show();
+                                    }
+                                }
+                            });
+
+
+                            //departmentPush(title_value,"Notice by HOD ".concat(dataSnapshot.child("name").getValue().toString()),Dept,downloadUrl.toString());
+
+                        }
+
+                        @Override
+                        public void onCancelled(DatabaseError databaseError) {
+
+                        }
+                    });
+
+
+                    //mProgress.dismiss();
+                    dialog.dismiss();
+                    //startActivity(new Intent(context , AddNoticeTabbed.class));
+                }
+            });
+
+        }
+        else if (imgPreview.getDrawable() == null) {
+            Toasty.error(context,"No image to upload").show();
+        }
     }
 
     @Override
@@ -140,9 +291,9 @@ public class AddImageNoticeFragment extends Fragment  {
             // When an Image is picked
             if (requestCode == Gallery_Request && resultCode == Activity.RESULT_OK
                     && null != data) {
-                Uri selectedImage = data.getData();
+                Uri mImageUri = data.getData();
 
-                CropImage.activity(selectedImage)
+                CropImage.activity(mImageUri)
                         .setGuidelines(CropImageView.Guidelines.ON)
                         .start(getContext(),this);
             }
@@ -151,10 +302,11 @@ public class AddImageNoticeFragment extends Fragment  {
                 CropImage.ActivityResult result = CropImage.getActivityResult(data);
                 Log.d("APP_DEBUG",result.toString());
                 if (resultCode == Activity.RESULT_OK) {
-                    Uri resultUri = result.getUri();
-                    Log.d("APP_DEBUG",resultUri.toString());
-                    Bitmap bitmap =  MediaStore.Images.Media.getBitmap(context.getContentResolver(), resultUri);
-                    imgPreview.setImageBitmap(bitmap);
+                    mImageUri = result.getUri();
+                    imgPreview.setImageURI(mImageUri);
+                    Log.d("APP_DEBUG",mImageUri.toString());
+                    //Bitmap bitmap =  MediaStore.Images.Media.getBitmap(context.getContentResolver(), resultUri);
+                    //imgPreview.setImageBitmap(bitmap);
 
                 } else if (resultCode == CropImage.CROP_IMAGE_ACTIVITY_RESULT_ERROR_CODE) {
                     Exception error = result.getError();
