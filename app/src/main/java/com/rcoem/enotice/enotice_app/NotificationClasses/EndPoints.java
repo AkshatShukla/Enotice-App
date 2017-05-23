198fddb1ae4971f9365a9430ecdaaf98485f7fdf
AkshatShukla <aks.shukla47@gmail.com>
2017-05-23 12:02:38 +0530

    Updated EndPoints as External Server migrated
    - Changed from polarisrcoem.in domain to rusartmnu.info, by migrating
    the external database and the PHP files to the new domain.

--- app/src/main/java/com/rcoem/enotice/enotice_app/NotificationClasses/EndPoints.java
+++ app/src/main/java/com/rcoem/enotice/enotice_app/NotificationClasses/EndPoints.java
@@ -5,13 +5,21 @@ package com.rcoem.enotice.enotice_app.NotificationClasses;
  */
 
 public class EndPoints {
-    public static final String URL_REGISTER_DEVICE = "http://polarisrcoem.in/RegisterDevice.php";
-    public static final String URL_SEND_SINGLE_PUSH = "http://polarisrcoem.in/sendSinglePush.php";
-    public static final String URL_SEND_MULTIPLE_PUSH = "http://polarisrcoem.in/sendMultiplePush.php";
-    public static final String URL_FETCH_DEVICES = "http://polarisrcoem.in/GetRegisteredDevices.php";
+    //public static final String URL_REGISTER_DEVICE = "http://polarisrcoem.in/RegisterDevice.php";
+    //public static final String URL_SEND_SINGLE_PUSH = "http://polarisrcoem.in/sendSinglePush.php";
+    //public static final String URL_SEND_MULTIPLE_PUSH = "http://polarisrcoem.in/sendMultiplePush.php";
+    //public static final String URL_FETCH_DEVICES = "http://polarisrcoem.in/GetRegisteredDevices.php";
 
+    public static final String URL_REGISTER_DEVICE = "http://rusartmnu.info/RegisterDevice.php";
+    public static final String URL_SEND_SINGLE_PUSH = "http://rusartmnu.info/sendSinglePush.php";
+    public static final String URL_SEND_MULTIPLE_PUSH = "http://rusartmnu.info/sendMultiplePush.php";
+    public static final String URL_FETCH_DEVICES = "http://rusartmnu.info/GetRegisteredDevices.php";
 
-    public static final String URL_SEND_SINGLE_PUSH_DEPT = "http://polarisrcoem.in/sendSinglePushDept.php";
-    public static final String URL_SEND_SINGLE_PUSH_HOD = "http://polarisrcoem.in/sendSinglePushHOD.php";
-    public static final String URL_SEND_SINGLE_PUSH_AUTHORITY = "http://polarisrcoem.in/sendSinglePushAuthority.php";
+    //public static final String URL_SEND_SINGLE_PUSH_DEPT = "http://polarisrcoem.in/sendSinglePushDept.php";
+    //public static final String URL_SEND_SINGLE_PUSH_HOD = "http://polarisrcoem.in/sendSinglePushHOD.php";
+    //public static final String URL_SEND_SINGLE_PUSH_AUTHORITY = "http://polarisrcoem.in/sendSinglePushAuthority.php";
+
+    public static final String URL_SEND_SINGLE_PUSH_DEPT = "http://rusartmnu.info/sendSinglePushDept.php";
+    public static final String URL_SEND_SINGLE_PUSH_HOD = "http://rusartmnu.info/sendSinglePushHOD.php";
+    public static final String URL_SEND_SINGLE_PUSH_AUTHORITY = "http://rusartmnu.info/sendSinglePushAuthority.php";
 }
